1834671d9d563eb68a65845d4e647b8b5ad860b2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-06 22:56:44 +0530

    user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,27 +1,34 @@
 import mongoose from "mongoose";
 
-
-
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-    },
-    username : {
-        type : String,
-        required : true,
-        unique : true,
-    },
-    email : {
-        type : String,
-        required: true,
-        unique : true,
-    },
-    password : {
-        type : String,
-        required : true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  active : {
+    type : Boolean,
+    default : true,
+  },
+  profilePicture : {
+    type : String,
+    default : "default.jpg",
+  },
+  create
+});
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 export const User;
