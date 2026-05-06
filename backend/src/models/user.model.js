99191343f773aab190ff811a51558284c0a025dc
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-06 22:57:16 +0530

    user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -27,7 +27,10 @@ const userSchema = new mongoose.Schema({
     type : String,
     default : "default.jpg",
   },
-  create
+  created_at : {
+    type : Date,
+    default : Date.now(),
+  }
 });
 
 const User = mongoose.model("User", userSchema);
