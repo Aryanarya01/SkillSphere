a535637ea0a94a7fb34f06bd20d02a80f53ab9c4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-06 22:48:27 +0530

    user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -7,7 +7,15 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true,
     },
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+    },
     email : {
-        
-    }
+        type : String,
+        required: true,
+        unique : true,
+    },
+
 })
