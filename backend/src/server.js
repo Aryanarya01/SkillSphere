ae580cb1802172736adeb7cc703a776b5a6d9b9e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-06 22:29:11 +0530

    server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,11 @@
 import express from "express"
-
+import cookieParser from "cookie-parser"
 const app = express();
 const port = 9090;
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser())
+
+
+const startDB = async()=>{
+    
+}
