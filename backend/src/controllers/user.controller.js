5708dc2ef1846988f986a4560bb28de0dc1210ac
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 15:36:27 +0530

    user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { use } from "react";
+ 
 import User from "../models/user.model";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
@@ -14,7 +14,7 @@ export const Register = async (req, res) => {
         return res.status(404).json({message : "User Already exists!"});
     }
     const hashedPassword = await bcrypt.hash(password,10);
-    const newUser = User.create({
+    const newUser = await User.create({
         name,
         username,
         email,
@@ -43,7 +43,11 @@ export const Login = async(req,res)=>{
             return res.status(403).json({message : "Invalid credientials"})
         }
         const token = jwt.sign({id : user._id});
-        res.cookie()
+        res.cookies("token",token,{
+            httpOnly : true,
+            
+        })
+        res.status(200).json({message : "Login successful"})
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
