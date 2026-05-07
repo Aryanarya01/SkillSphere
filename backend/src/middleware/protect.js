e7bc7ae8b62f49e7f1cebf25192ba0b59a7dd45e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:24:29 +0530

    user controller

--- backend/src/middleware/protect.js
+++ backend/src/middleware/protect.js
@@ -1,1 +1,11 @@
-export const Protect
+export const Protect = async(req,res)=>{
+    try{
+        const token = req.cookies;
+        if(!token){
+            return res.status(404).json({message : "Token not found!"});
+        }
+        
+    }catch(err){
+        return res.status(500).json({message : "Server Error!"})
+    }
+}
