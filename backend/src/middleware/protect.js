1dd614168534bdd08713c3109e29b3d9cb5f8f93
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:27:56 +0530

    user controller

--- backend/src/middleware/protect.js
+++ backend/src/middleware/protect.js
@@ -1,5 +1,6 @@
 
 import jwt from "jsonwebtoken"
+import User from "../models/user.model";
 
 export const Protect = async(req,res)=>{
     try{
@@ -8,7 +9,11 @@ export const Protect = async(req,res)=>{
             return res.status(404).json({message : "Token not found!"});
         }
         const decoded = jwt.verify(token,JWT_SECRET);
-        const user 
+        const user = await User.findById(decoded.id).select("-password");
+        if(!user){
+            return res.status(404).json({message : "User not found"})
+        }
+        
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
