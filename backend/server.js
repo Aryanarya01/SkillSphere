63c222bce69fa6f76a9a1b19295a6dd6194bbf7d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:33:04 +0530

    User route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import cors from "cors"
+import userRouter from "./src/controllers/user.controller.js"
+
+
 const app = express();
 const port = 9090;
 
@@ -10,7 +13,7 @@ dotenv.config()
 app.use(cors())
 app.use(express.json());
 app.use(cookieParser())
-
+app.use()
 
 const startDB = async()=>{
     const connect  = await mongoose.connect("mongodb+srv://aryanarya01:aryan5555@skillsphere.jcqnrhp.mongodb.net/?appName=SkillSphere")
