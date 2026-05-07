2b7cbb754b1b5e799cd32c1190676ea14919c73e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 21:48:24 +0530

    role middleware

--- backend/src/middleware/role.js
+++ backend/src/middleware/role.js
@@ -1,15 +1,13 @@
-
-
-export const authorizedRole = (...roles)=>{
-    return async (req,res,next)=>{
-    if(!req.user){
-        return res.status(401).json({message : "Unauthorized"})
+export const authorizedRole = (...roles) => {
+  return async (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    if(!roles.include(req.user.role)){
-        return res.status(403).json({
-            message : "Access Denide"
-        })
+    if (!roles.include(req.user.role)) {
+      return res.status(403).json({
+        message: "Access Denide",
+      });
     }
-    next()
-}
+    next();
+  };
 };
