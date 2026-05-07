06ccbe3a01c1bc08611cbbf288b59f772c449269
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:31:42 +0530

    User route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { Register } from "../controllers/user.controller";
+import { Login, Register } from "../controllers/user.controller";
 
 const router = Router();
 
-router.route("/register").post(Register)
+router.route("/register").post(Register);
+router.route("/login").post(Login);
+
+
+export default router;
