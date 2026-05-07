53c219399261f8d63dbe7b1f70f8af6fb607f34e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:28:14 +0530

    user controller

--- backend/src/middleware/protect.js
+++ backend/src/middleware/protect.js
@@ -2,7 +2,7 @@
 import jwt from "jsonwebtoken"
 import User from "../models/user.model";
 
-export const Protect = async(req,res)=>{
+export const Protect = async(req,res,next)=>{
     try{
         const token = req.cookies.token;
         if(!token){
@@ -13,7 +13,7 @@ export const Protect = async(req,res)=>{
         if(!user){
             return res.status(404).json({message : "User not found"})
         }
-        
+
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
