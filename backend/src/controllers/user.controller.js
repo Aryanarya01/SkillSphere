d11d844e09d9ac8a989f82f5b37f1d454723d9f1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 15:11:01 +0530

    user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -38,7 +38,10 @@ export const Login = async(req,res)=>{
         if(!user){
         return res.status(404).json({message : "User not found!"});
     }
-        const 
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(403).json({message : "Invalid credientials"})
+        }
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
