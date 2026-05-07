f9def5515bacca367e5b799034b9710f4738e46c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:26:47 +0530

    user controller

--- backend/src/middleware/protect.js
+++ backend/src/middleware/protect.js
@@ -1,10 +1,14 @@
+
+import jwt from "jsonwebtoken"
+
 export const Protect = async(req,res)=>{
     try{
-        const token = req.cookies;
+        const token = req.cookies.token;
         if(!token){
             return res.status(404).json({message : "Token not found!"});
         }
-        
+        const decoded = jwt.verify(token,JWT_SECRET);
+        const user 
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
