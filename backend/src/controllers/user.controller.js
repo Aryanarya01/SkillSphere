39914fa5737b3a59582755f23bc309d9db4b71df
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 15:09:18 +0530

    user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -38,6 +38,7 @@ export const Login = async(req,res)=>{
         if(!user){
         return res.status(404).json({message : "User not found!"});
     }
+        const 
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
