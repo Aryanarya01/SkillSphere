f0d9daca105dc618c6adf1dc6e2b5584fd41de18
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 16:33:08 +0530

    User route

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import dotenv from "dotenv"
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors"
-import userRouter from "./src/controllers/user.controller.js"
-
+import cors from "cors";
+import userRouter from "./src/controllers/user.controller.js";
 
 const app = express();
 const port = 9090;
 
-dotenv.config()
-app.use(cors())
+dotenv.config();
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
-app.use()
+app.use(cookieParser());
+app.use();
 
-const startDB = async()=>{
-    const connect  = await mongoose.connect("mongodb+srv://aryanarya01:aryan5555@skillsphere.jcqnrhp.mongodb.net/?appName=SkillSphere")
-    app.listen(port,()=>{
-        console.log(`Server is listining to port ${port}`)
-    })
-
-}
+const startDB = async () => {
+  const connect = await mongoose.connect(
+    "mongodb+srv://aryanarya01:aryan5555@skillsphere.jcqnrhp.mongodb.net/?appName=SkillSphere",
+  );
+  app.listen(port, () => {
+    console.log(`Server is listining to port ${port}`);
+  });
+};
 startDB();
