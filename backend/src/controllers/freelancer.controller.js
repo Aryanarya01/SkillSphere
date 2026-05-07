4b15ce474915aec8987d678994edbe7a4aa6b24e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 23:27:04 +0530

    freelancer controller

--- backend/src/controllers/freelancer.controller.js
+++ backend/src/controllers/freelancer.controller.js
@@ -1,18 +1,16 @@
-import Freelancer from "../models/freelancer.model.js"
+import Freelancer from "../models/freelancer.model.js";
 
-
-
-export const getMyFreelancerProfile = async(req,res)=>{
-    try{
-        const freelancer = await Freelancer.findOne({
-            user : req.user._id
-        }).populate("user","-password");
-        if(!freelancer){
-            return res.status(404).json({message : "Freelancer not found"})
-        }
-
-        return res.status(200).json({freelancer})
-    }catch(err){
-        return res.status(500).json({message : err.message})
+export const getMyFreelancerProfile = async (req, res) => {
+  try {
+    const freelancer = await Freelancer.findOne({
+      user: req.user._id,
+    }).populate("user", "-password");
+    if (!freelancer) {
+      return res.status(404).json({ message: "Freelancer not found" });
     }
-}
+
+    return res.status(200).json({ freelancer });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
