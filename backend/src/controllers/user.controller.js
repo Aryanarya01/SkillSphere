88ec7777d8f6ddc3e962183253015de1083086a9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 15:30:22 +0530

    user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -37,12 +37,13 @@ export const Login = async(req,res)=>{
         const user = await User.findOne({email})
         if(!user){
         return res.status(404).json({message : "User not found!"});
-    }
+     }
         const isMatch = await bcrypt.compare(password,user.password);
         if(!isMatch){
             return res.status(403).json({message : "Invalid credientials"})
         }
-        const token = jwt.sign({id : user._id})
+        const token = jwt.sign({id : user._id});
+        res
     }catch(err){
         return res.status(500).json({message : "Server Error!"})
     }
