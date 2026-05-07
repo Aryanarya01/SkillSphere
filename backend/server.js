b4e771b57eae374bac6bfb97c80a1e6e45f64986
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-07 22:00:03 +0530

    api.http

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import userRouter from "./src/controllers/user.controller.js";
+import userRouter from "./src/routes/user.routes.js";
 
 const app = express();
 const port = 9090;
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use();
+app.use(userRouter);
 
 const startDB = async () => {
   const connect = await mongoose.connect(
