464479650f3151eae8eedd7cdb50014aeaa3bee0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-08 21:28:45 +0530

    freelancer route

--- backend/src/routes/freelancer.route.js
+++ backend/src/routes/freelancer.route.js
@@ -1,10 +1,12 @@
 import express, { Router } from "express"
 import { Protect } from "../middleware/protect.js";
-import { authorizedRole } from "../middleware/role";
+import { authorizedRole } from "../middleware/role.js";
+import { getMyFreelancerProfile } from "../controllers/freelancer.controller";
 const router = Router();
 
 
-router.route("/my-profile").get(Protect,authorizedRole())
+router.route("/my-profile").get(Protect,authorizedRole("freelancer"),getMyFreelancerProfile);
+
 
 
 export default router
