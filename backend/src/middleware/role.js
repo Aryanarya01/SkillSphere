c932f805a89e98de10b5bd7543f000a9a5329057
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 17:15:15 +0530

    freelancer route

--- backend/src/middleware/role.js
+++ backend/src/middleware/role.js
@@ -1,13 +1,18 @@
 export const authorizedRole = (...roles) => {
   return async (req, res, next) => {
+
     if (!req.user) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-    if (!roles.include(req.user.role)) {
-      return res.status(403).json({
-        message: "Access Denide",
+      return res.status(401).json({
+        message: "Unauthorized",
       });
     }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Access Denied",
+      });
+    }
+
     next();
   };
 };
