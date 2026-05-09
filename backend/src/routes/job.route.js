1a913e049ac7c4be8a0c278da4eaf47b637e2a8d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 18:40:45 +0530

    job route

--- backend/src/routes/job.route.js
+++ backend/src/routes/job.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { createJob } from "../controllers/job.controller";
 
 
 const router = Router();
 
-
+router.route("/create_job").post(createJob);
+route
 
 
 export default router;
