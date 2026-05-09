f84ce2321408aa0480dfa9d3384133bae5ea61d2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 22:14:04 +0530

    proposal model

--- backend/src/models/proposal.model.js
+++ backend/src/models/proposal.model.js
@@ -1,38 +1,35 @@
 import mongoose from "mongoose";
 
+const proposalSchema = new mongoose.Schema(
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    freelancer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    coverLetter: {
+      type: String,
+      required: true,
+    },
+    bidAmount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-
-const proposalSchema = new mongoose.Schema({
-    job : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Job",
-        required : true,
-    },
-    freelancer : {
-        type : mongoose.model.Types.ObjectId,
-        ref : "User",
-        required : true,
-    },
-    coverLetter : {
-        type : String,
-        required : true,
-    },
-    bidAmount : {
-        type : Number,
-        required : true,
-    },
-    status : {
-        type : String,
-        enum : [
-            "pending",
-        "accepted",
-        "rejected",
-        ],
-        default: "pending",
-    }
-},{
-    timestamps : true
-})
-
-const Proposal = mongoose.model("Proposal",proposalSchema);
+const Proposal = mongoose.model("Proposal", proposalSchema);
 export const Proposal;
