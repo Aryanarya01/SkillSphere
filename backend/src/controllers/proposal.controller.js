6e7c2b6cd2ef8259c21026e08dfcf6a95e1fe087
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 22:29:34 +0530

    proposal controller aplly job

--- backend/src/controllers/proposal.controller.js
+++ backend/src/controllers/proposal.controller.js
@@ -3,6 +3,8 @@
 //apply job
 export const applyJob = async(req,res)=>{
     try{
+        const {coverLetter, bidAmount} = req.body;
+        const JobId = req.params.JobId;
         
     }catch(err){
         return res.status(500).json({message : err.message})
