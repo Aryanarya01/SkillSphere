4e9e3d5d2afea4fd343b5f37c7d5eb12ec208212
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 18:55:26 +0530

    job route

--- backend/src/routes/job.route.js
+++ backend/src/routes/job.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createJob, deleteJob, getAllJobs, getSingleJob, updateJob } from "../controllers/job.controller.js";
 import { Protect } from "../middleware/protect.js";
+import { authorizedRole } from "../middleware/role.js";
 
 
 const router = Router();
 
-router.route("/create_job").post(Protect,createJob);
+router.route("/create_job").post(Protect,authorizedRole("client"),createJob);
 router.route("/get_all_job").get(Protect,getAllJobs);
 router.route("/get_single_job").get(Protect,getSingleJob);
 router.route("/update_job").put(Protect,updateJob);
