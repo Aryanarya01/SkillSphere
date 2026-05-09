ad2006bb675019ebf7fb97127208f4ff343fbe19
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 18:10:13 +0530

    freelancer route

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -2,7 +2,12 @@
 
 export const createJob = async(req,res)=>{
     try{
-
+        const {title,
+      description,
+      budget,
+      skillsRequired,
+      deadline} = req.body;
+       
     }catch(err){
         return res.status(500).json({message : err.message})
     }
