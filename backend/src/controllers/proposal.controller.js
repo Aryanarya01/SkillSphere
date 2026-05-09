860337933fe97564cdbc763eaf7ae3a65625296c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 22:33:44 +0530

    proposal controller aplly job

--- backend/src/controllers/proposal.controller.js
+++ backend/src/controllers/proposal.controller.js
@@ -1,4 +1,5 @@
 import Job from "../models/job.model.js";
+import { Proposal } from "../models/proposal.model.js";
 
 
 //apply job
@@ -12,7 +13,10 @@ export const applyJob = async(req,res)=>{
         if(!job){
             return res.status(404).json({message : "Job not found!"});
         }
-        
+        const existingProposal = await Proposal.findOne({
+            job : JobId,
+            freelancer : req.user._id,
+        })
     }catch(err){
         return res.status(500).json({message : err.message})
     }
