8db127d757fdb110ee0dc5a7bfa7627fa7cf5922
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 18:18:42 +0530

    freelancer route

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -31,7 +31,7 @@ export const createJob = async (req, res) => {
 
 export const getAllJobs = async (req,res)=>{
     try{
-
+        const jobs = await Job.find().populate("client","-password")
     }catch(err){
         return res.status(500).json({message : err.message})
     }
