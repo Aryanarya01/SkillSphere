7e0ab8ad788c56611137c753d5fa20daf15ef6a8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 18:10:30 +0530

    freelancer route

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -7,7 +7,15 @@ export const createJob = async(req,res)=>{
       budget,
       skillsRequired,
       deadline} = req.body;
-       
+        if (
+      !title ||
+      !description ||
+      !budget
+    ) {
+      return res.status(400).json({
+        message: "Required fields missing",
+      });
+    }
     }catch(err){
         return res.status(500).json({message : err.message})
     }
