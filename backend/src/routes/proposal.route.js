ef13d02beb2e2b41ab4bf100b32956d04c2167cc
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 23:48:56 +0530

    proposal route

--- backend/src/routes/proposal.route.js
+++ backend/src/routes/proposal.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { Protect } from "../middleware/protect";
 
 
 const router = Router();
 
-
+router.route("/proposal/:JobId").post(Protect)
 
 export default router;
