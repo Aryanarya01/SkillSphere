3b3a804e330cf8547875e1d8d6eb7e7ee0169aff
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 21:43:04 +0530

    job route

--- backend/src/routes/job.route.js
+++ backend/src/routes/job.route.js
@@ -6,11 +6,11 @@ import { authorizedRole } from "../middleware/role.js";
 
 const router = Router();
 
-router.route("/create_job").post(Protect,authorizedRole("client"),createJob);
-router.route("/get_all_job").get(Protect,getAllJobs);
-router.route("/get_single_job").get(Protect,getSingleJob);
-router.route("/update_job").put(Protect,updateJob);
-router.route("/delete_job").delete(Protect,deleteJob)
+router.route("/jobs/").post(Protect,authorizedRole("client"),createJob);
+router.route("/jobs/").get(Protect,getAllJobs);
+router.route("/jobs/").get(Protect,getSingleJob);
+router.route("/jobs/").put(Protect,updateJob);
+router.route("/jobs/").delete(Protect,deleteJob)
 
 
 export default router;
