4eec7c19a4625e46009d5a7e78183a71af1d1147
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 23:54:24 +0530

    proposal route

--- backend/src/routes/proposal.route.js
+++ backend/src/routes/proposal.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
 import { authorizedRole } from "../middleware/role.js";
-import { applyJob } from "../controllers/proposal.controller.js";
+import { applyJob, getJobProposals } from "../controllers/proposal.controller.js";
 
 
 const router = Router();
 
-router.route("/proposal/:JobId").post(Protect,authorizedRole("freelancer"),applyJob);
-
+router.route("/proposal/apply/:JobId").post(Protect,authorizedRole("freelancer"),applyJob);
+router.route("/proposal/job/:JobId").get(Protect, authorizeRoles("client"),getJobProposals)
 
 export default router;
