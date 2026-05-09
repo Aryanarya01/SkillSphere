9609fa095c1bbc81becc59af9e2dcd526f58a294
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 22:09:26 +0530

    proposal model

--- backend/src/models/proposal.model.js
+++ backend/src/models/proposal.model.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 
 
 const proposalSchema = new mongoose.Schema({
-
+    job : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Job",
+        required : true,
+    },
+    free
 })
 
 const Proposal = mongoose.model("Proposal",proposalSchema);
