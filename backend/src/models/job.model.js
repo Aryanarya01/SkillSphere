a4f5146167d3cb3411940b19e393d6618bfd787d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-09 18:04:48 +0530

    freelancer route

---
+++ backend/src/models/job.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+
+const jobSchema = new mongoose.Schema({
+ client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    budget: {
+      type: Number,
+      required: true,
+    },
+
+    skillsRequired: [
+      {
+        type: String,
+      },
+    ],
+
+    deadline: {
+      type: Date,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "open",
+        "in-progress",
+        "completed",
+      ],
+      default: "open",
+    },
+  },
+  { timestamps: true }
+)
