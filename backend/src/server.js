df16dc271fb5e28801f4c29fb251b63a971161c6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 23:24:46 +0530

    Login

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,10 @@ const app = express();
 const port = 9090;
 
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
