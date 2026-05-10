e58e889d1165449c33565babd3a02fcc47e883a9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 23:39:54 +0530

    Register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,13 +2,13 @@
 
 
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Register = () => {
 
     const dispatch = useDispatch();
+    const {isLoading} = useSelector((state)=>state.auth)
     
-
   return (
     <div>Register</div>
   )
