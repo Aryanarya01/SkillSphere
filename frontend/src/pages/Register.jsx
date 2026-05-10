badc807f1758f085defa275e0fab0e6ffd009044
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 23:38:45 +0530

    Register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,11 +2,13 @@
 
 
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 const Register = () => {
 
-
+    const dispatch = useDispatch();
     
+
   return (
     <div>Register</div>
   )
