13fe2d8e4e8c259e846287828ba3c9bc0569b680
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 14:55:54 +0530

    review route

--- backend/src/routes/review.route.js
+++ backend/src/routes/review.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
-import { createReview, getUserReview } from "../controllers/review.controller.js";
-
+import {
+  createReview,
+  getUserReview,
+} from "../controllers/review.controller.js";
 
 const router = Router();
 
-router.route("/reviews/create").post(Protect,createReview)
-router.route("/review/:userId").get(Protect,getUserReview);
+router.route("/reviews/create").post(Protect, createReview);
+router.route("/review/:userId").get(Protect, getUserReview);
 export default router;
