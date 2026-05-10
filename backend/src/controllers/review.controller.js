822649d61cd1e9c141e986c874d434aef010a95e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 09:57:05 +0530

    review controller

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -1,9 +1,19 @@
+import { Review } from "../models/reviews.model.js";
 
 
 
 export const createReview = async(req,res)=>{
     try{
+        const {receiver,job,rating,comment} = req.body;
+        const review = await Review.create({
+            reviewer : req.user._id,
+            receiver,
+            job,
+            rating,
+            comment
+        })
 
+        return
     }catch(err){
         return res.status(500).json({message : err.message})
     }
