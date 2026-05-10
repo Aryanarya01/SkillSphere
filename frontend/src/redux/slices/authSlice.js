0a8a359058ce6436464e1de5e091deaa3d17614a
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 16:08:50 +0530

    slice

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,36 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user : null,
-    isLoading : false,
-}
+  user: null,
+  isLoading: false,
+};
 
 const auhtSlice = createSlice({
-    name : "auth",
-    initialState,
-    reducers : {
+  name: "auth",
+  initialState,
+  reducers: {
+    //login
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
 
-        //login
-        setUser : (state, action)=>{
-            state.user = action.payload;
-        },
+    //logout
+    logoutUser: (state) => {
+      state.user = null;
+    },
 
+    //Loading
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+  },
+});
 
-        //logout
-        logoutUser : (state)=>{
-            state.user = null;
-        },
-
-
-        //Loading
-        setLoading : (state,action)=>{
-            state.isLoading = action.payload;
-        },
-
-
-    }
-})
-
-
-export const {setUser, logoutUser, setLoading} = auhtSlice.actions;
+export const { setUser, logoutUser, setLoading } = auhtSlice.actions;
 export default auhtSlice.reducer;
