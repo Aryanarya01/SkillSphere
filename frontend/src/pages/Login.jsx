d75d41aa1bf919f160df97ce8bee26363ac64e6c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 18:28:57 +0530

    Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,9 +7,50 @@ const Login = () => {
     email : "",
     password : "",
   });
-  
+
   return (
-    <div>Login</div>
+     <div className="flex justify-center items-center h-screen">
+
+      <form
+       
+        className="flex flex-col gap-4 w-[300px] border p-5 rounded"
+      >
+
+        <h1 className="text-2xl font-bold text-center">
+          Login
+        </h1>
+
+        {/* EMAIL */}
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          value={formData.email}
+          
+          className="border p-2 rounded"
+        />
+
+        {/* PASSWORD */}
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={formData.password}
+        
+          className="border p-2 rounded"
+        />
+
+        {/* BUTTON */}
+        <button
+          type="submit"
+          className="bg-black text-white p-2 rounded"
+        >
+          Login
+        </button>
+
+      </form>
+
+    </div>
   )
 }
 
