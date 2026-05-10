3f3476252da7258724028a56c746acb93f1f0a50
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 14:54:53 +0530

    review route

--- backend/src/routes/review.route.js
+++ backend/src/routes/review.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { Protect } from "../middleware/protect.js";
+import { createReview } from "../controllers/review.controller.js";
 
 
 const router = Router();
 
-
+router.route("/reviews/create").post(Protect,createReview)
 
 export default router;
