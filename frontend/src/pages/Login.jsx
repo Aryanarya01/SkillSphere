03eda41c818a5d675b6fec9b3efbd5834838917b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 18:22:48 +0530

    Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useDispatch} from "react-redux"
 const Login = () => {
   const dispatch = useDispatch();
 
-
+  const [formData, setFormData] = useState({
+    email : "",
+    password : "",
+  });
+  
   return (
     <div>Login</div>
   )
