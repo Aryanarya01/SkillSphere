00ac9c5169f3e9b475b6e59cd8aeb005fdc560fd
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 09:27:53 +0530

    reviews model

--- backend/src/models/reviews.model.js
+++ backend/src/models/reviews.model.js
@@ -1,34 +1,36 @@
 import mongoose from "mongoose";
 
+export const reviewSchema = new mongoose.Schema(
+  {
+    reviewer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-export const reviewSchema = new mongoose.Schema({
-    reviewer : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true,
-    },
-    receiver : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true,
-    },
-    job : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Job",
-        required : true
-    },
-    rating : {
-        type : Number,
-        required : true,
-        min : 1,
-        max : 5
-    },
-    comment : {
-        type : String,
-    },
-},{
-    timestamps : true
-})
-
-const Review = mongoose.model("Review",reviewSchema);
+const Review = mongoose.model("Review", reviewSchema);
 export const Review;
