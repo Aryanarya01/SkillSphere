639141b45fed963b5d257c0165173330898f707b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-10 16:01:39 +0530

    slice

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,5 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     user : null,
     isLoading : false,
 }
+
+const auhtSlice = createSlice({
+    name : "auth",
+    initialState,
+    
+})
