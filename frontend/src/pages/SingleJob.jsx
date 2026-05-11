2cedc5dec1cfc08a7a8671671952b50f04adf9e3
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 23:02:53 +0530

    singleJob

--- frontend/src/pages/SingleJob.jsx
+++ frontend/src/pages/SingleJob.jsx
@@ -1,56 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import clientServer from '../api/client.js';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import clientServer from "../api/client.js";
+import { useParams } from "react-router-dom";
 
 const SingleJob = () => {
+  const [job, setJob] = useState(null);
+  const { id } = useParams();
 
-    const [job,setJob] = useState(null);
-    const {id} = useParams();
-
-    const fetchJob = async()=>{
-        try{
-            const res = await clientServer.get(`/jobs/${id}`);
-            setJob(res.data.job);
-
-        }catch(err){
-            console.log(err);
-        }
+  const fetchJob = async () => {
+    try {
+      const res = await clientServer.get(`/jobs/${id}`);
+      setJob(res.data.job);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(()=>{
-        fetchJob();
-    },[])
+  useEffect(() => {
+    fetchJob();
+  }, []);
 
-    if(!job){
-        return (
-            <div className='p-10'>
-                Loading...
-            </div>
-        )
-    }
+  if (!job) {
+    return <div className="p-10">Loading...</div>;
+  }
 
   return (
-    <div className='min-h-screen bg-gray-100 p-6'>
-         <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-lg p-8">
-                  <h1 className="text-4xl font-bold text-gray-800">
-          {job.title}
-        </h1>    
-         <p className="text-gray-600 mt-6 leading-relaxed">
-          {job.description}
-        </p>
-         <div className="mt-6">
-          <span className="font-bold text-xl">
-            Budget:
-          </span>
-          <span className="ml-2 text-lg">
-            ₹ {job.budget}
-          </span>
-          </div>   
-          
-               
-         </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-lg p-8">
+        <h1 className="text-4xl font-bold text-gray-800">{job.title}</h1>
+        <p className="text-gray-600 mt-6 leading-relaxed">{job.description}</p>
+        <div className="mt-6">
+          <span className="font-bold text-xl">Budget:</span>
+          <span className="ml-2 text-lg">₹ {job.budget}</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleJob
+export default SingleJob;
