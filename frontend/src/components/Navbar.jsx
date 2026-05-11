445e3d4820773101bd134833683872b3842cb35d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 18:03:02 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
 const handelLogout = async()=>{
     try{
-        await clientServer.get
+        await clientServer.get("/logout")
     }catch(err){
         console.log(err);
         
