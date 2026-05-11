365e9aa52be9cb38d09d2f26e05fb5db3d6bd2a4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 18:19:26 +0530

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,62 +1,57 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import clientServer from '../api/client.js';
-import { logoutUser } from '../redux/slices/authSlice.js';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import clientServer from "../api/client.js";
+import { logoutUser } from "../redux/slices/authSlice.js";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const {user} = useSelector((state)=>state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
 
-
-const handelLogout = async()=>{
-    try{
-        await clientServer.get("/logout");
-        dispatch(logoutUser());
-        navigate("/login");
-    }catch(err){
-        console.log(err);
+  const handelLogout = async () => {
+    try {
+      await clientServer.get("/logout");
+      dispatch(logoutUser());
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
     }
-}
+  };
 
   return (
-      <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
-
-        <Link
-        to="/"
-        className="text-2xl font-bold"
-      >
+    <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
+      <Link to="/" className="text-2xl font-bold">
         SkillSphere
       </Link>
 
-        <div className='flex items-center gap-5'>
-            {
-                user ? (
-                    <>
-                    <Link to="/dashboard">
-                        Dashboard
-                    </Link>
-                    <p>{user.name}</p>
-                    <button className="bg-white text-black px-4 py-2 rounded-lg" onClick={handelLogout}>Logout</button>
-                    </>
-                ) : (
-                    <>
-                        <Link to="/login">
-                            Login
-                        </Link>   
+      <div className="flex items-center gap-5">
+        {user ? (
+          <>
+            <Link to="/dashboard">Dashboard</Link>
+            <p>{user.name}</p>
+            <button
+              className="bg-white text-black px-4 py-2 rounded-lg"
+              onClick={handelLogout}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
 
-                        <Link  className="bg-white text-black px-4 py-2 rounded-lg" to="/register">
-                            Register
-                        </Link>
-                    </>
-                )
-            }
-        </div>
+            <Link
+              className="bg-white text-black px-4 py-2 rounded-lg"
+              to="/register"
+            >
+              Register
+            </Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-      </nav>
-
-  )
-}
-
-export default Navbar
+export default Navbar;
