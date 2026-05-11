b9559e3f947d7b1c68781712de3c3efd5393b1de
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 23:02:50 +0530

    singleJob

--- frontend/src/pages/SingleJob.jsx
+++ frontend/src/pages/SingleJob.jsx
@@ -34,7 +34,20 @@ const SingleJob = () => {
          <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-lg p-8">
                   <h1 className="text-4xl font-bold text-gray-800">
           {job.title}
-        </h1>            
+        </h1>    
+         <p className="text-gray-600 mt-6 leading-relaxed">
+          {job.description}
+        </p>
+         <div className="mt-6">
+          <span className="font-bold text-xl">
+            Budget:
+          </span>
+          <span className="ml-2 text-lg">
+            ₹ {job.budget}
+          </span>
+          </div>   
+          
+               
          </div>
     </div>
   )
