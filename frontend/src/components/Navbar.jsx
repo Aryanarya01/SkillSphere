90a03c158ab529a60b4666f4eb2ea36febda484c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 17:50:58 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
       <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
 
+        <Link
+        to="/"
+        className="text-2xl font-bold"
+      >
+        SkillSphere
+      </Link>
+
         
+
       </nav>
 
   )
