7512a5738b63bceae8c7b57c7cfcb296fb4fd93c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 18:36:23 +0530

    home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <section>
+    <div className="min-h-screen bg-gray-100">
+      <section className="flex flex-col justify-center items-center text-center py-28 px-6">
         <h1>Find Top Freelancers & Build Amazing Projects</h1>
         <p>  SkillSphere helps clients connect with
           talented freelancers around the world.</p>
