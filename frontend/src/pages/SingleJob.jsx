607b49bf923eeff6a34c7edb64994b9154b9b807
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 22:53:32 +0530

    singleJob

--- frontend/src/pages/SingleJob.jsx
+++ frontend/src/pages/SingleJob.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SingleJob = () => {
+
+    const [job,setJob] = useState(null)
+
   return (
     <div>SingleJob</div>
   )
