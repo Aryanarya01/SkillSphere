eefdf98d2be69359f19bb4902e602f46e2ab4269
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 21:17:17 +0530

    Job.jsx

--- frontend/src/pages/Job.jsx
+++ frontend/src/pages/Job.jsx
@@ -4,6 +4,14 @@ const Job = () => {
 
     const [jobs,setJobs] = useState([]);
 
+    const fetchJobs = async()=>{
+        try{
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
   return (
     <div className='min-h-screen bg-gray-100 p-6'> 
         <div >
