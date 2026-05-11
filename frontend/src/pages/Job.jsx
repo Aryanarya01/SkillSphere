57b11f4499efe543acc12ac2c52158aad794bf70
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-11 21:16:41 +0530

    Job.jsx

--- frontend/src/pages/Job.jsx
+++ frontend/src/pages/Job.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Job = () => {
+
+    const [jobs,setJobs] = useState([]);
+
   return (
     <div className='min-h-screen bg-gray-100 p-6'> 
-        <div>
+        <div >
+            <h1>Available Jobs</h1>
+            <p> Explore freelance opportunities</p>
+        </div>
 
+        <div>
+            {
+                
+            }
         </div>
     </div>
   )
