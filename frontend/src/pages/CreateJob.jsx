2937b36f8471f9b2e988ddc7046d401b50829d63
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-12 22:05:02 +0530

    CreateJob

--- frontend/src/pages/CreateJob.jsx
+++ frontend/src/pages/CreateJob.jsx
@@ -9,8 +9,22 @@ const CreateJob = () => {
 
         {/* heading */}
         <div>
-          <h1></h1>
+          <h1>Post a New Job</h1>
+          <p>Find the best freelancers for your work</p>
         </div>
+
+
+      <form> 
+
+        {/* title */}
+      <div>
+        <label>Job Title</label>
+        <input type="text" placeholder="Enter job title" />
+      </div>
+
+      </form>
+
+
       </div>
     </div>
   )
