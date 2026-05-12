fcc7764fbd4332132a4067f686d41044f7c91065
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-12 22:27:36 +0530

    CreateJob

--- frontend/src/pages/CreateJob.jsx
+++ frontend/src/pages/CreateJob.jsx
@@ -29,7 +29,8 @@ const handelSubmit = async(e)=>{
       skillsRequired : formData.skillsRequired.split(",").map((skill)=>skill.trim())
     }
     const res = await clientServer.post("/jobs/create",mainRes);
-
+    alert(res.data.message);
+    navigate("/jobs");
   }catch(err){
     console.log(err);
   }
