3a8a6a4dbda9feadab9a0b9e60c6818660dc3760
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 23:42:11 +0530

    admin controller

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,8 +1,8 @@
 
 
-export const AdminAnalysis = async(req,res)=>{
+export const AdminAnalysisData = async(req,res)=>{
     try{
-        
+        const total
     }catch(err){
         return res.status(500).json({message : err.message})
     }
