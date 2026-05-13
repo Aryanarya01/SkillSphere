b96b204160d9e668e6eefd074473018445195322
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 09:42:28 +0530

    viewProposals

--- frontend/src/pages/ViewProposals.jsx
+++ frontend/src/pages/ViewProposals.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
+import clientServer from "../api/client.js";
+import { useParams } from 'react-router-dom';
 
 const ViewProposals = () => {
 
     const [proposals, setProposals]  = useState([]);
-
+  const {JobId}= useParams()
   const fetchPropsals = async()=>{
     try{
-
+      const res = await clientServer.get(`/proposal/job/${JobId}`)
     }catch(err){
       console.log(err);
-      
     }
   }
   return (
