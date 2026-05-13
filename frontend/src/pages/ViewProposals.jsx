98fae18bcdccc8599c185844401409a39c273377
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 09:39:32 +0530

    viewProposals

--- frontend/src/pages/ViewProposals.jsx
+++ frontend/src/pages/ViewProposals.jsx
@@ -3,8 +3,15 @@ import React, { useState } from 'react'
 const ViewProposals = () => {
 
     const [proposals, setProposals]  = useState([]);
-    
 
+  const fetchPropsals = async()=>{
+    try{
+
+    }catch(err){
+      console.log(err);
+      
+    }
+  }
   return (
     <div>ViewProposals</div>
   )
