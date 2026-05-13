060e23fa3ade6943f64a0f4d220f01e8b8484785
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 23:07:58 +0530

    protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,51 @@
- import React from 'react'
-import Login from './pages/Login.jsx'
- import "./index.css";
-import Register from './pages/Register.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard.jsx';
-import Navbar from './components/Navbar.jsx';
-import Home from './pages/Home.jsx';
-import Job from './pages/Job.jsx';
-import SingleJob from './pages/SingleJob.jsx';
-import CreateJob from './pages/CreateJob.jsx';
-import ClientDashboard from './pages/ClientDashboard.jsx';
-import ViewProposals from './pages/ViewProposals.jsx';
-import FreelancerDashboard from './pages/FreelancerDashboard.jsx';
-import ProtectedRoutes from './components/ProtectedRoutes.jsx';
- 
+import React from "react";
+import Login from "./pages/Login.jsx";
+import "./index.css";
+import Register from "./pages/Register.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Job from "./pages/Job.jsx";
+import SingleJob from "./pages/SingleJob.jsx";
+import CreateJob from "./pages/CreateJob.jsx";
+import ClientDashboard from "./pages/ClientDashboard.jsx";
+import ViewProposals from "./pages/ViewProposals.jsx";
+import FreelancerDashboard from "./pages/FreelancerDashboard.jsx";
+import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
 
- const App = () => {
-   return (
+const App = () => {
+  return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/dashboard' element={<Dashboard/>} />
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/jobs' element={<Job/>}/>
-          <Route path='/jobs/:id' element={<SingleJob/>} />
-          <Route path='/jobs/create' element={<CreateJob/>}/>
-          <Route path='/client-dashboard' element={ <ProtectedRoutes role="client">
-            <ClientDashboard/>
-          </ProtectedRoutes>}/>
-          <Route path='/proposals/:JobId' element={<ViewProposals/>}/>
-          <Route path='/freelancer-dashboard' element={<ProtectedRoutes role="freelancer">
-            <FreelancerDashboard/>
-          </ProtectedRoutes>}/>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/jobs" element={<Job />} />
+        <Route path="/jobs/:id" element={<SingleJob />} />
+        <Route path="/jobs/create" element={<CreateJob />} />
+        <Route
+          path="/client-dashboard"
+          element={
+            <ProtectedRoutes role="client">
+              <ClientDashboard />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/proposals/:JobId" element={<ViewProposals />} />
+        <Route
+          path="/freelancer-dashboard"
+          element={
+            <ProtectedRoutes role="freelancer">
+              <FreelancerDashboard />
+            </ProtectedRoutes>
+          }
+        />
       </Routes>
     </BrowserRouter>
-   )
- }
- 
- export default App
+  );
+};
+
+export default App;
