383461671c5eeaee0c89cd1776f3be0f6e2f61c8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 23:44:20 +0530

    admin controller

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,20 +1,19 @@
 import Job from "../models/job.model.js";
 import Proposal from "../models/proposal.model.js";
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
 
+export const AdminAnalysisData = async (req, res) => {
+  try {
+    const totalUser = await User.countDocuments();
+    const totalJobs = await Job.countDocuments();
+    const totalProposals = await Proposal.countDocuments();
 
-export const AdminAnalysisData = async(req,res)=>{
-    try{
-        const totalUser = await User.countDocuments();
-        const totalJobs = await Job.countDocuments();
-        const totalProposals = await Proposal.countDocuments();
-
-        return res.status(200).json({
-            totalUser,
-            totalJobs,
-            totalProposals
-        })
-    }catch(err){
-        return res.status(500).json({message : err.message})
-    }
-}
+    return res.status(200).json({
+      totalUser,
+      totalJobs,
+      totalProposals,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
