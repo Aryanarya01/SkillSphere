3172eb8e53d5efcc32c7f6c5d88a228e041d7f58
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 09:49:31 +0530

    viewProposals

--- frontend/src/pages/ViewProposals.jsx
+++ frontend/src/pages/ViewProposals.jsx
@@ -1,47 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import clientServer from "../api/client.js";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 const ViewProposals = () => {
-
-    const [proposals, setProposals]  = useState([]);
-  const {JobId}= useParams()
-  const fetchPropsals = async()=>{
-    try{
+  const [proposals, setProposals] = useState([]);
+  const { JobId } = useParams();
+  const fetchPropsals = async () => {
+    try {
       const res = await clientServer.get(`/proposal/job/${JobId}`);
       setProposals(res.data.proposal);
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  useEffect(()=>{ 
+  useEffect(() => {
     fetchPropsals();
-  },[])
+  }, []);
 
-  const handelAccept = async(id)=>{
-    try{  
+  const handelAccept = async (id) => {
+    try {
       const res = await clientServer.put(`/proposal/accept/${id}`);
       alert(res.data.proposal);
       fetchPropsals();
-    }catch(err){
-      console.log(err);      
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
-  const handelReject = async(id)=>{
-    try{
+  const handelReject = async (id) => {
+    try {
       const res = await clientServer.put(`/proposal/reject/${id}`);
       setProposals(res.data.proposal);
-      fetchPropsals()
-    }catch(err){
-      console.log(err);      
+      fetchPropsals();
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   return (
-    <div>ViewProposals</div>
-  )
-}
+      <div>
+        <div>
+          <h1 className="text-4xl font-bold">
+          Job Proposals
+        </h1>
 
-export default ViewProposals
+        <p className="text-gray-500 mt-2">
+          Manage freelancer applications
+        </p>
+        </div>
+      </div>
+  );
+};
+
+export default ViewProposals;
