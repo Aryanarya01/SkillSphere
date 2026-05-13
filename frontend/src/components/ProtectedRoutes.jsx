035ca602e2a55b8f36762d43f07fc781c67a8ee5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-13 23:01:50 +0530

    protected routes

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,10 +1,13 @@
 
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ProtectedRoutes = ({children, role}) => {
+    const {user} = useSelector((state)=>state.auth);
+    
   return (
     <>
-        
+
     </>
   )
 }
