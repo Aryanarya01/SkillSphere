74691ae6e173ae9b0675757e065d79eb5e0e9c86
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 17:04:19 +0530

    Admindashboard

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
 import { authorizedRole } from "../middleware/role.js";
-import { AdminAnalysisData, getAllUser } from "../controllers/admin.controller.js";
+import { AdminAnalysisData, deleteUser, getAllUser } from "../controllers/admin.controller.js";
 
 const router = Router();
 
 router.route("/admin/stats").get(Protect, authorizedRole("admin"), AdminAnalysisData);
 router.route("admin/users").get(Protect,authorizedRole("admin"),getAllUser);
-router
+router.route("/admin/user/:id").delete(Protect,authorizedRole("admin"),deleteUser);
+
 export default router;
