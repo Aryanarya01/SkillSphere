6edb5d47899833ea0b154c3121079efa12404a52
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 21:10:06 +0530

    manageJobs

--- frontend/src/pages/ManageJobs.jsx
+++ frontend/src/pages/ManageJobs.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ManageJobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async()=>{
+    try{
+
+    }catch(err){
+      
+    }
+  }
+
   return (
     <div>ManageJobs</div>
   )
