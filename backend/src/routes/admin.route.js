73709aa4b49316a881f3b1247f62f2bcc4f05269
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 18:46:04 +0530

    manageuser

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
 import { authorizedRole } from "../middleware/role.js";
-import { AdminAnalysisData, deleteUser, getAllUser } from "../controllers/admin.controller.js";
+import { AdminAnalysisData, deleteJobAdmin, deleteUser, getAllJobsAdmin, getAllUser } from "../controllers/admin.controller.js";
 
 const router = Router();
 
 router.route("/admin/stats").get(Protect, authorizedRole("admin"), AdminAnalysisData);
 router.route("/admin/users").get(Protect,authorizedRole("admin"),getAllUser);
 router.route("/admin/user/:id").delete(Protect,authorizedRole("admin"),deleteUser);
+router.route("/admin/jobs").get(Protect,authorizedRole("admin"),getAllJobsAdmin);
+router.route("admin/job/:id").delete(Protect,authorizedRole("admin"),deleteJobAdmin);
+
 
 export default router;
