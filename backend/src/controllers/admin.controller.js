242e46481716a2b0f068f9b248bd2608b927afe2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 15:44:08 +0530

    Admindashboard

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -23,6 +23,7 @@ export const AdminAnalysisData = async (req, res) => {
 export const getAllUser = async(req,res)=>{
   try{
     const users = await User.find().select("-password");
+    return res.status(200).json({users});
     
   }catch(err){
     return res.status(500).json({message : err.message})
