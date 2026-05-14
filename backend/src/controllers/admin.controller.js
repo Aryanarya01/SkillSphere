98b5033a3871497bade20337189bef95dfa80f43
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 18:43:05 +0530

    manageuser

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -38,10 +38,19 @@ export const deleteUser = async(req,res)=>{
 }
 
 
-export const getAllJobs = async(req,res)=>{
+export const getAllJobsAdmin = async(req,res)=>{
   try{
     const jobs = await Job.find().populate("client","name email");
     return res.status(200).json({jobs})
+  }catch(err){
+    return res.status(500).json({message : err.message})
+  }
+}
+
+
+export const deleteJobAdmin = async(req,res)=>{
+  try{
+
   }catch(err){
     return res.status(500).json({message : err.message})
   }
