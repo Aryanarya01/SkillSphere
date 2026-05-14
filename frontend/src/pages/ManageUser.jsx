b5a1cb2d328e9978dd727fb56ad53b7776967c71
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 17:44:36 +0530

    manageuser

--- frontend/src/pages/ManageUser.jsx
+++ frontend/src/pages/ManageUser.jsx
@@ -1,70 +1,64 @@
-
-
-import React from 'react'
-import clientServer from '../api/client.js';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import clientServer from "../api/client.js";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ManageUser = () => {
+  const [users, setUsers] = useState([]);
 
-    const [users,setUsers] = useState([]);
-
-
-    const fetchUser = async()=>{
-        try{
-            const res = await clientServer.get("admin/users");
-            setUsers(res.data.users)
-        }catch(err){
-            console.log(err);
-        }
+  const fetchUser = async () => {
+    try {
+      const res = await clientServer.get("admin/users");
+      setUsers(res.data.users);
+    } catch (err) {
+      console.log(err);
     }
-    useEffect(()=>{
-        fetchUser();
-    },[]);
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    const handelDelete = async(id)=>{
-        try{
-            await clientServer.delete(`/admin/user/${id}`);
-            fetchUser();
-        }catch(err){
-            console.log(err);
-        }
+  const handelDelete = async (id) => {
+    try {
+      await clientServer.delete(`/admin/user/${id}`);
+      fetchUser();
+    } catch (err) {
+      console.log(err);
     }
+  };
   return (
-    <div className='min-h-screen bg-gray-100 p-6'>
-        
-        <div className='mb-8'>
-            <h1 className='text-4xl font-bold'>Manage Users</h1>
-            <p className='text-gray-500 mt-2'>Admin user management panel</p>
-        </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="mb-8">
+        <h1 className="text-4xl font-bold">Manage Users</h1>
+        <p className="text-gray-500 mt-2">Admin user management panel</p>
+      </div>
 
-    <div className='bg-white rounded-2xl shadow-md overflow-hidden'>
+      <div className="bg-white rounded-2xl shadow-md overflow-hidden">
         <table>
-            <thead>
-                <tr> 
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    users.map((user)=>(
-                        <tr>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.role}</td>
-                            <td><button onClick={()=>handelDelete(user._id)}>Delete</button></td>
-                        </tr>
-                    ))
-                }
-            </tbody>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  <button onClick={() => handelDelete(user._id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default ManageUser
+export default ManageUser;
