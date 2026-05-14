4a7f1fd3cbb4dba3b1a0da767e639cf6e9f37622
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 15:32:55 +0530

    Admindashboard

--- frontend/src/pages/AdminDashoboard.jsx
+++ frontend/src/pages/AdminDashoboard.jsx
@@ -35,8 +35,8 @@ useEffect(()=>{
     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
 
     <div className='bg-white rounded-2xl shadow-md p-6'>
-        <h2>Total Users</h2>
-        <p>{stats.totalUser}</p>
+        <h2 className='text-gray-500 text-lg'>Total Users</h2>
+        <p className='text-5xl font-bold mt-4'>{stats.totalUser}</p>
     </div>
 
     </div>
