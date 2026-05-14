b790034bed631656ea4b1200d1fc88370ea667cb
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 21:16:21 +0530

    manageJobs

--- frontend/src/pages/ManageJobs.jsx
+++ frontend/src/pages/ManageJobs.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-
+import clientServer from "../api/client.js";
 const ManageJobs = () => {
   const [jobs, setJobs] = useState([]);
 
   const fetchJobs = async()=>{
     try{
-      const res = await
+      const res = await clientServer.get("/admin/jobs");
+      setJobs(res.data.jobs)
     }catch(err){
       console.log(err);
     }
