d54318fe87ef60fca81c8a126576e4e9d957d515
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 08:47:47 +0530

    admin route

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
 import { authorizedRole } from "../middleware/role.js";
+import { AdminAnalysisData } from "../controllers/admin.controller.js";
 
 
 const router = Router();
 
-router.route("/stats").get(Protect,authorizedRole("admin"),)
+router.route("/stats").get(Protect,authorizedRole("admin"),AdminAnalysisData)
 
 export default router;
