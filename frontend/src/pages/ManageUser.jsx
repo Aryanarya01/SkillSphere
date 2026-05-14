687ce7dedf9423d2bbe9639391a88caedc2c54a0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 17:12:41 +0530

    manageuser

--- frontend/src/pages/ManageUser.jsx
+++ frontend/src/pages/ManageUser.jsx
@@ -1,16 +1,26 @@
 
 
 import React from 'react'
+import clientServer from '../api/client.js';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const ManageUser = () => {
 
+    const [users,setUsers] = useState([]);
+
+
     const fetchUser = async()=>{
         try{
-
+            const res = await clientServer.get("admin/users");
+            setUsers(res.data.users)
         }catch(err){
             console.log(err);
         }
     }
+    useEffect(()=>{
+        fetchUser();
+    },[])
   return (
     <div>ManageUser</div>
   )
