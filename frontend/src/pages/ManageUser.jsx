6f70ecaca88183c461f62e5ffc0dd7d7e6d6b486
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-14 17:10:10 +0530

    manageuser

--- frontend/src/pages/ManageUser.jsx
+++ frontend/src/pages/ManageUser.jsx
@@ -3,6 +3,14 @@
 import React from 'react'
 
 const ManageUser = () => {
+
+    const fetchUser = async()=>{
+        try{
+
+        }catch(err){
+            console.log(err);
+        }
+    }
   return (
     <div>ManageUser</div>
   )
