3d5b42ab9172d29a59526e85fde085b2478ccde3
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-15 22:12:40 +0530

    getAllJobs

--- frontend/src/pages/Job.jsx
+++ frontend/src/pages/Job.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 const Job = () => {
   const [jobs, setJobs] = useState([]);
-
+  const [keyword, setkeyword] = useState("");
+  
   const fetchJobs = async () => {
     try {
       const res = await clientServer.get("/jobs");
