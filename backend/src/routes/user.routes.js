6f7c16f6ef6583b9fc277d43d2c9399a9d3cc56e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-15 18:07:45 +0530

    upload middleware

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { Login, Logout, Register } from "../controllers/user.controller.js";
+import { Login, Logout, Register, updateProfile } from "../controllers/user.controller.js";
 import {Protect} from "../middleware/protect.js"
+import upload from "../middleware/upload.middleware.js";
 const router = Router();
 
 router.route("/register").post(Register);
 router.route("/login").post(Login);
 router.route("/logout").get(Protect,Logout);
+router.route("/update-profile",Protect,upload.single("profilePicture"),updateProfile)
 export default router;
