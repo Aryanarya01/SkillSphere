4d8cbdb717a66e73c4ed386dc04c214cce6492e7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-15 15:51:14 +0530

    profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,11 @@
 
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+const {user} = useSelector((state)=>state.auth);
+
+
   return (
     <div>Profile</div>
   )
