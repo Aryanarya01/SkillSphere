d4d62edf623f0a5c36ad1ca7d8274fb0e1b3cf31
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-15 17:52:00 +0530

    upload middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -94,8 +94,22 @@ export const updateProfile = async(req,res)=>{
     const {name, username} = req.body;
     const user = await User.findById(req.user._id);
     if(!user){
-      return res.status(404).json({message : ""})
+      return res.status(404).json({message : "User not found"})
     }
+    // updatefield
+
+    user.name = name || user.name;
+    user.username = username || user.username;
+
+    if(req.file){
+      user.profilePicture =  `/uploads/${req.file.filename}`;
+    }
+    await user.save();
+       return res.status(200).json({
+        message:
+          "Profile updated successfully",
+        user,
+      });
   }catch(err){
     return res.status(500).json({message : err.message})
   }
