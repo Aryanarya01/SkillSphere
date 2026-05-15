90aa3fe5af49591bf4b1fa3e7952a93ecfa63546
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-15 08:48:06 +0530

    manageJobs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import { Toaster } from "react-hot-toast";
 import App from './App.jsx'
 import "./index.css";
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 createRoot(document.getElementById('root')).render(
    <Provider store={store}>
+      <Toaster
+    position="top-right"
+  />
     <App/>
    </Provider>
 )
