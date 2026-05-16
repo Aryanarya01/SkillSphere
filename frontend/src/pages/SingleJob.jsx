18373fc1d493a1de2cf7f9340f3abcaec95306e6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-16 18:30:53 +0530

    getAllJobs

--- frontend/src/pages/SingleJob.jsx
+++ frontend/src/pages/SingleJob.jsx
@@ -59,7 +59,8 @@ const SingleJob = () => {
 
   const handleSaveJob = async()=>{
     try{
-
+      const res = clientServer.post(`/jobs/save/${id}`);
+      toast.success(res.data.message)
     }catch(err){
       toast.error(err.response?.data?.message)
     }
