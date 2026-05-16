3f84ef474ae61c34cc59f815f7fdd1f5b302e210
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-16 23:51:14 +0530

    review form

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
+import clientServer from '../api/client';
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
 
     const fetchReviews = async()=>{
         try{
-
+            const res = await clientServer.get
         }catch(err){
             console.log(err);
             toast.err("Error fetching");
