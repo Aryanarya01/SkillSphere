668428f3fcf59d0d1543890decb683e2a3aea33c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-17 18:20:05 +0530

    review form

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -38,7 +38,7 @@ const Reviews = ({ userId }) => {
             {/* Reviewer */}
             <div className="flex items-center gap-4">
               <img
-                src={`${BASE_URL}${review.reviewer?.profilePicture}`}
+                src={`http://localhost:9090${review.reviewer?.profilePicture}`}
                 alt="reviewer"
                 className="w-14 h-14 rounded-full object-cover"
               />
