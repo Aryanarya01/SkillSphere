7fb3e3831d6c769245ebfe7462bb2ec221909491
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-18 12:04:14 +0530

    starting working on notification

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,8 +1,9 @@
+import Notification from "../models/notification.model.js"
 
 
 export const getNotification = async(req,res)=>{
     try{
-        
+        const notifications = await Notification.fin
     }catch(err){
         return res.status(500).json({message : err.message})
     }
