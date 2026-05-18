ac015d87e7b80abab63946b36333506738fc1c14
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-18 12:06:34 +0530

    starting working on notification

--- backend/src/models/notification.model.js
+++ backend/src/models/notification.model.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
-const notificationSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
   },
-  message: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  read: {
-    type: Boolean,
-    default: false,
-  },
-},{
-    timestamps : true
-});
+);
 
 const Notification = mongoose.model("Notification", notificationSchema);
 export default Notification;
