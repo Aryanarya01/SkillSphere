882915e4dcf1286eac6780f7b2d5662dade760ac
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-18 12:17:49 +0530

    notification page

--- frontend/src/pages/NotificationPage.jsx
+++ frontend/src/pages/NotificationPage.jsx
@@ -1,13 +1,15 @@
  
-import React from 'react'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const NotificationPage = () => {
-
+    const [notifications,setNotifications] = useState([])
+    
     const fetchNotification = async ()=>{
         try{
 
         }catch(err){
-            
+            toast.error("Error")
         }
     }
 
