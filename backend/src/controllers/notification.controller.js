e5fb248d078a198510b7c56010572f80aa1e245d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-18 12:06:27 +0530

    starting working on notification

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -6,8 +6,12 @@ export const getNotification = async(req,res)=>{
         const notifications = await Notification.find({
             user : req.user._id,
         }).sort({
-            
+            createdAt: -1,
         })
+          return res.status(200).json({
+        notifications,
+      });
+
     }catch(err){
         return res.status(500).json({message : err.message})
     }
