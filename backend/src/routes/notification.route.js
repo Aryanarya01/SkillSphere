72e6742cc7d1bb56a7f563936b88330635e091d0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-18 12:37:55 +0530

    notification completed

--- backend/src/routes/notification.route.js
+++ backend/src/routes/notification.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
-import { getNotification } from "../controllers/notification.controller.js";
+import { getNotification, markAsRead } from "../controllers/notification.controller.js";
 
 
 const router = Router();
 
 
 router.route("/notifications").get(Protect,getNotification)
+router.route("/notifications/read").put(Protect,markAsRead);
 
 export default router;
