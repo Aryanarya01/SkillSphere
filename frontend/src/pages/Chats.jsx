fd2514b65ac248069b32a46419b42a4fa2ac5019
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 17:30:41 +0530

    chat.jsx

--- frontend/src/pages/Chats.jsx
+++ frontend/src/pages/Chats.jsx
@@ -5,7 +5,15 @@ import React, { useState } from 'react'
 
 const Chats = () => {
     const [messages, setMessages] = useState([]);
-    
+    const [text,setText] = useState("");
+
+    const fetchMessages = async()=>{
+        try{
+
+        }catch(err){
+            console.log(err)
+        }
+    }
   return (
     <div>Chats</div>
   )
