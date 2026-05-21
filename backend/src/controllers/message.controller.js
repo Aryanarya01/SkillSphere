f42325df4422a836135aeecc017b7d54d24cc064
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 12:48:57 +0530

    message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,9 @@
+import { Message } from "../models/message.model.js";
 
 export const sendMessage = async(req,res)=>{
     try{
-        
+        const {reciever, text} = req.body;
+        const message = await Message.
     }catch(err){
         return res.status(500).json({message : err.message})
     }
