b6dddd1238c2b458a5daf693c9e834e1e8bc34d2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 15:58:10 +0530

    message model

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { sendMessage } from "../controllers/message.controller.js";
-
+import {Protect} from "../middleware/protect.js"
 
 const router = Router();
 
-router.route("messages/send").post(sendMessage)
+router.route("messages/send").post(Protect,sendMessage)
 
 export default router;
