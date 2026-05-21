8a135e37a587cab781d67e4b8017a4c3411882a6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 12:18:41 +0530

    proposal accepted immediately

--- frontend/src/pages/FreelancerDashboard.jsx
+++ frontend/src/pages/FreelancerDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import clientServer from "../api/client.js";
+import socket from "../socket.js";
 
 const FreelancerDashboard = () => {
   const [proposals, setProposals] = useState([]);
@@ -16,6 +17,14 @@ const FreelancerDashboard = () => {
 
   useEffect(() => {
     fetchPropsals();
+    socket.on("proposalUpdated",(data)=>{
+      setProposals((prev)=>prev.map((proposal)=>
+        proposal._id === data.proposalId ? {
+          ...proposal,status : data.status,
+        } : proposal
+      ))
+    })
+    return ()
   }, []);
 
   return (
