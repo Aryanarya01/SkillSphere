ba8f6b88b552658ddf2e4c99c545237df5e93f27
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 17:33:11 +0530

    chat.jsx

--- frontend/src/pages/Chats.jsx
+++ frontend/src/pages/Chats.jsx
@@ -2,14 +2,14 @@
 
 
 import React, { useState } from 'react'
-
+import clientServer from "../api/client.js"
 const Chats = () => {
     const [messages, setMessages] = useState([]);
     const [text,setText] = useState("");
 
     const fetchMessages = async()=>{
         try{
-            const messages = await clientSer
+            const messages = await clientServer.get(`/messages/${id}`)
         }catch(err){
             console.log(err)
         }
