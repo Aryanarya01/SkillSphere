939ae030ea8e6c5068f14157275db9fb2dfe41f4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 22:34:40 +0530

    getConversations

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import { getConversations, getMessages, sendMessage } from "../controllers/message.controller.js";
 import { Protect } from "../middleware/protect.js";
 
 const router = Router();
 
 router.route("/messages/send").post(Protect, sendMessage);
 router.route("/messages/:id").get(Protect, getMessages);
-
+router.route("/messages/conversations/all").get(Protect,getConversations);
 export default router;
