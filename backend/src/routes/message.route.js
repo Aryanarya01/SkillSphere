02b894a6604eefc5f2db1cccfd03679306ceea9b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-21 15:57:11 +0530

    message model

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { sendMessage } from "../controllers/message.controller.js";
 
 
 const router = Router();
 
+router.route("messages/send").post(sendMessage)
 
 export default router;
