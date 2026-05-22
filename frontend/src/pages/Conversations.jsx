05519cf3af039082b5f95622d492700d3d5c0399
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-22 09:45:38 +0530

    Conversation.jsx

--- frontend/src/pages/Conversations.jsx
+++ frontend/src/pages/Conversations.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Conversations = () => {
+    const [conversations, setConversations] = useState([]);
+    
   return (
-    <div>Conversations</div>
+    <div className='w-80 bg-white border-r h-full overflow-y-auto'>
+        <div className='p-5 border-b'>
+            <h2 className='text-2xl font-bold'>Chats</h2>
+        </div>
+
+        {/* users */}
+        <div >
+
+        </div>
+    </div>
   )
 }
 
