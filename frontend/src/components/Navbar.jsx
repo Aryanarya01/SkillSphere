6f5f0529c0aec31bfd5a5a2f8195bc2122abd94d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-22 17:51:36 +0530

    Conversation.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   
   const [count, setCount] = useState(0);
- 
+  const [isOpen, setIsOpen] = useState(false)
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
