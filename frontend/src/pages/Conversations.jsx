dbd9985b57eea8bd1eade63aed4fd454f32511ba
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-22 10:08:30 +0530

    Conversation.jsx

--- frontend/src/pages/Conversations.jsx
+++ frontend/src/pages/Conversations.jsx
@@ -1,45 +1,51 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
-import clientServer from '../api/client.js';
-import { useEffect } from 'react';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import clientServer from "../api/client.js";
+import { useEffect } from "react";
 
 const Conversations = () => {
-    const [conversations, setConversations] = useState([]);
-    const fetchConversations = async()=>{
-        try{
-            const res = await clientServer.get("/messages/conversations/all");
-            setConversations(res.data.conversations)
-        }catch(err){
-            console.log(err)
-        }
+  const [conversations, setConversations] = useState([]);
+  const fetchConversations = async () => {
+    try {
+      const res = await clientServer.get("/messages/conversations/all");
+      setConversations(res.data.conversations);
+    } catch (err) {
+      console.log(err);
     }
-    useEffect(()=>{
-        fetchConversations();
-    },[])
+  };
+  useEffect(() => {
+    fetchConversations();
+  }, []);
   return (
-    <div className='w-80 bg-white border-r h-full overflow-y-auto'>
-        <div className='p-5 border-b'>
-            <h2 className='text-2xl font-bold'>Chats</h2>
-        </div>
+    <div className="w-80 bg-white border-r h-full overflow-y-auto">
+      <div className="p-5 border-b">
+        <h2 className="text-2xl font-bold">Chats</h2>
+      </div>
 
-        {/* users */}
-        <div className='space-y-2 p-3' >
-            {
-                conversations.map((user)=>(
-                    <Link key={user._id} to={`/chat/${user._id}`} className='flex items-center gap-4 p-3 hover:bg-gray-100 rounded-xl transition'>
-                        <img src={`http://localhost:9090${user.profilePicture}`} alt="profile" className='w-12 h-12 rounded-full object-cover' />
-                        {/* information */}
-                        <div>
-                            <h3>{user.name}</h3>
-                            <p>Open Chat</p>
-                        </div>
-                    </Link>
-                ))
-            }
-        </div>
+      {/* users */}
+      <div className="space-y-2 p-3">
+        {conversations.map((user) => (
+          <Link
+            key={user._id}
+            to={`/chat/${user._id}`}
+            className="flex items-center gap-4 p-3 hover:bg-gray-100 rounded-xl transition"
+          >
+            <img
+              src={`http://localhost:9090${user.profilePicture}`}
+              alt="profile"
+              className="w-12 h-12 rounded-full object-cover"
+            />
+            {/* information */}
+            <div>
+              <h3>{user.name}</h3>
+              <p>Open Chat</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Conversations
+export default Conversations;
