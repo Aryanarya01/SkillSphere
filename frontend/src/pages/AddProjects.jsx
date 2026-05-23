77a04193bf27eae383db0f3fd26440d16112e82d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 12:42:49 +0530

    addProjects

--- frontend/src/pages/AddProjects.jsx
+++ frontend/src/pages/AddProjects.jsx
@@ -1,8 +1,19 @@
 
 
 import React from 'react'
+import { useState } from 'react'
 
 const AddProjects = () => {
+    const [formData, setFormData] = useState({
+
+      title: "",
+      description: "",
+      technologies: "",
+      githubLink: "",
+      liveLink: "",
+    });
+
+    const [image, setImage] = useState(null)
   return (
     <div>AddProjects</div>
   )
