9f53251980cf3aa353ec0139dc4e0b74568674c5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 12:01:36 +0530

    portfolio routes

--- backend/src/routes/portfolio.route.js
+++ backend/src/routes/portfolio.route.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
-
+import {Protect} from "../middleware/protect.js";
+import upload from "../middleware/upload.middleware.js"
 const router = Router();
 
-
+router.route("portfolio/create").post(Protect,uploa)
 export default router;
