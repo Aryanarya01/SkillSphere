668b305625420e2a18488a33c7b2738f960feed1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 16:32:33 +0530

    addProjects

--- frontend/src/pages/AddProjects.jsx
+++ frontend/src/pages/AddProjects.jsx
@@ -1,41 +1,42 @@
-
-
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const AddProjects = () => {
-    const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    technology: "",
+    githubLink: "",
+    liveLink: "",
+  });
 
-      title: "",
-      description: "",
-      technology: "",
-      githubLink: "",
-      liveLink: "",
-    });
-
-    const [image, setImage] = useState(null)
+  const [image, setImage] = useState(null);
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center p-6">
-         <div className="w-full max-w-2xl bg-white rounded-2xl shadow-xl p-8">
-             <h1 className="text-3xl font-bold mb-8">
+      <div className="w-full max-w-2xl bg-white rounded-2xl shadow-xl p-8">
+        <h1 className="text-3xl font-bold mb-8">Add Project</h1>
 
-          Add Project
-
-        </h1>
-
-        <form >
-            <input type="text" name='title' placeholder='Project Title' />
-            <textarea name="" id="" name='description' placeholder='Project Description'/>
-            <input type="text" name='technology'  placeholder="React, Node.js, MongoDB" />
-            <input type="text" name='githubLink' placeholder="Github Link" />
-            <input type="text" name='liveLink'  placeholder="Live Project Link" />
-            <input type="file" />
-            <button></button>
+        <form>
+          <input type="text" name="title" placeholder="Project Title" />
+          <textarea
+            name=""
+            id=""
+            name="description"
+            placeholder="Project Description"
+          />
+          <input
+            type="text"
+            name="technology"
+            placeholder="React, Node.js, MongoDB"
+          />
+          <input type="text" name="githubLink" placeholder="Github Link" />
+          <input type="text" name="liveLink" placeholder="Live Project Link" />
+          <input type="file" />
+          <button></button>
         </form>
-
-         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddProjects
+export default AddProjects;
