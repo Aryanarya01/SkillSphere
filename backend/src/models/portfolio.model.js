4d966bb82b4db0a07b02839f51fd25fdd477051d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 11:47:17 +0530

    portfolio

--- backend/src/models/portfolio.model.js
+++ backend/src/models/portfolio.model.js
@@ -3,6 +3,25 @@ import mongoose from "mongoose";
 
 const portfolioSchema = new mongoose.Schema({
     user : {
-        type: 
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+    },
+    title : {
+        type : String,
+        required : true,
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    technology : [
+        {
+            type : String
+        }
+    ],
+    githubLink : {
+        type : String,
+        default : ""
     }
 })
