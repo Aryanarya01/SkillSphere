bc66c87fd9e37e9c2e6c54e0fa8a44cdf169eded
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 11:55:45 +0530

    portfolio

--- backend/src/controllers/portfolio.controller.js
+++ backend/src/controllers/portfolio.controller.js
@@ -1,20 +1,19 @@
 import Portfolio from "../models/portfolio.model";
 
-
-export const createProject = async(req,res)=>{
-    try{
-        const {title, description, technology, githubLink, liveLink} = req.body;
-        const portfolio = await Portfolio.create({
-            user : req.user._id,
-            title,
-            description,
-            technology : technologies.split(","),
-            githubLink,
-            liveLink,
-            image : req.file  ? `/uploads/${req.file.filename}` : ""
-        });
-        return res.status(201).json({portfolio})
-    }catch(err){
-        return res.status(500).json({message : "Server Error"})
-    }
-}
+export const createProject = async (req, res) => {
+  try {
+    const { title, description, technology, githubLink, liveLink } = req.body;
+    const portfolio = await Portfolio.create({
+      user: req.user._id,
+      title,
+      description,
+      technology: technologies.split(","),
+      githubLink,
+      liveLink,
+      image: req.file ? `/uploads/${req.file.filename}` : "",
+    });
+    return res.status(201).json({ portfolio });
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
