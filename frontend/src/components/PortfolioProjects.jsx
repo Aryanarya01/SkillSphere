0a04c6c9b1062c38d1d2e9ac0a04e43ddb6ac3fb
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 18:14:41 +0530

    portfolioProjects

---
+++ frontend/src/components/PortfolioProjects.jsx
@@ -0,0 +1,18 @@
+
+import React, { useState } from 'react'
+
+const PortfolioProjects = () => {
+    const [projects, setProjects] = useState([]);
+    const fetchProjects = async()=>{
+        try{
+
+        }catch(err){
+            
+        }
+    }
+  return (
+    <div>PortfolioProjects</div>
+  )
+}
+
+export default PortfolioProjects
