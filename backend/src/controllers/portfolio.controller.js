95c23de2686a58effd076ad38d1428ed812b071a
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 11:52:14 +0530

    portfolio

--- backend/src/controllers/portfolio.controller.js
+++ backend/src/controllers/portfolio.controller.js
@@ -2,7 +2,7 @@
 
 export const createProject = async(req,res)=>{
     try{
-        const {} = req.body;
+        const {title, description, technology, githubLink, liveLink} = req.body;
     }catch(err){
         return res.status(500).json({message : "Server Error"})
     }
