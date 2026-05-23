201452e6e40c71a1e4b626cb74d7e8d4aa6a7630
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-23 18:16:48 +0530

    portfolioProjects

--- frontend/src/components/PortfolioProjects.jsx
+++ frontend/src/components/PortfolioProjects.jsx
@@ -1,13 +1,17 @@
 
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import clientServer from '../api/client.js';
 
-const PortfolioProjects = () => {
+const PortfolioProjects = ({userId}) => {
     const [projects, setProjects] = useState([]);
     const fetchProjects = async()=>{
         try{
-
+            const res = await clientServer.get(`/portfolio/${userId}`);
+            setProjects(res.data.projects);
         }catch(err){
-            
+            console.log(err);
+            toast.error("Error fetching projects.")
         }
     }
   return (
