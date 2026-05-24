011d06aae3273a5853f77f80312db466b57e5241
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-24 18:37:11 +0530

    userProfile

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("https://skillsphere-xt39.onrender.com/", {
+const socket = io("https://skillsphere-xt39.onrender.com", {
   withCredentials: true,
 });
 
