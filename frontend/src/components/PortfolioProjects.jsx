d3d4b816d9ffec1601c7f3e98726e69d890e1bd1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-24 17:40:08 +0530

    userProfile

--- frontend/src/components/PortfolioProjects.jsx
+++ frontend/src/components/PortfolioProjects.jsx
@@ -21,7 +21,7 @@ const PortfolioProjects = ({ userId }) => {
     <div className="mt-10">
       <h2 className="text-3xl font-bold mb-6">Portfolio Projects</h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1  gap-6">
         {projects.map((project) => (
           <div
             key={project._id}
