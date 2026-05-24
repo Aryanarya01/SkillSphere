a83ddac081c0a29d1c47903096c4f3c815a094c4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-24 18:25:03 +0530

    final feature

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -69,8 +69,8 @@ export const Login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({ message: "Login successful", user });
